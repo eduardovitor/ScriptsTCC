0fecef540d1a9a68723167363b34848f49b29a11
Eduardo Vitor <eduardovitor730@gmail.com>
2022-09-18 18:20:25 -0300

    Adição script de coleta das urls de arquivo html

---
+++ get_urls_list.py
@@ -0,0 +1,21 @@
+
+from bs4 import BeautifulSoup
+
+with open('resumo_dev_indice.html', 'r') as f:
+    contents = f.read()
+    soup = BeautifulSoup(contents, 'lxml')
+
+urls=soup.findAll('a')
+urls_cidades_fixed=[]
+for url in urls:
+    url_cidade=url['href']
+    url_cidade=url_cidade.replace('/portal/','')
+    url_cidade=url_cidade.replace('/transparencia/','')
+    url_cidade=url_cidade.replace('/transparencia','')
+    url_cidade=url_cidade.replace('/trans','')
+    urls_cidades_fixed.append(url_cidade)
+
+with open('lista_cidades.txt','w') as writer:
+    for url in urls_cidades_fixed:
+        writer.write(url+'\n')
+
