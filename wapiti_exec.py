32d66464408a2934d636e67e9b918063796246d5
Eduardo Vitor <eduardovitor730@gmail.com>
2022-09-21 20:34:55 -0300

    Adição dos timestamps e tempo de execução no arquivo json

--- wapiti_exec.py
+++ wapiti_exec.py
@@ -1,3 +1,4 @@
+import json
 import subprocess  
 from time import sleep
 from datetime import datetime
@@ -8,15 +9,22 @@ def wapiti(urls):
     len_urls=len(urls)
     while(i<len_urls):
         for k in range(1,4):
-            final_args='-f json -o ~/'+ urls[i] + '-' + k + '.json' + ' --flush-attacks'
+            final_args='-f json -o '+ urls[i] + '-' + k + '.json' + ' --flush-attacks'
             timestampInicio = datetime.now()
             temp = subprocess.run(cmd+ ' ' + '-u' + ' ' + urls[i] + ' ' + final_args)
             timestampFinal = datetime.now()
             duracao=timestampFinal-timestampInicio
-            duracaoMin=duracao.min
-            nomeArquivoDuracao=urls[i]+'-'+k+'-duracao'+'.txt'  # Talvez colocar esse dado no json logo
-            with open(nomeArquivoDuracao, 'w') as writer:
-                writer.write(duracaoMin)
+            new_data={
+                "start_timestamp": timestampInicio,
+                "final_timestamp": timestampFinal,
+                "duration_minutes" : duracao.min
+            }  
+            filename= urls[i] + '-' + k + '.json'
+            with open(filename,'r+') as file:
+                file_data = json.load(file)
+                file_data["infos"].append(new_data)
+                file.seek(0)
+                json.dump(file_data, file, indent = 4)
             sleep(120)
             if k==3:
                 i+=1
@@ -29,3 +37,4 @@ for i in range(len(urls)):
     urls[i] = urls[i].strip('\n')
 
 wapiti(urls)
+
