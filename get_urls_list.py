18639c418cf44a17a394038ea50594e7fe86c57a
Eduardo Vitor <eduardovitor730@gmail.com>
2022-09-24 15:22:54 -0300

    Modificando script para substituir /trans/

--- get_urls_list.py
+++ get_urls_list.py
@@ -13,6 +13,7 @@ for url in urls:
     url_cidade=url_cidade.replace('/transparencia/','')
     url_cidade=url_cidade.replace('/transparencia','')
     url_cidade=url_cidade.replace('/trans','')
+    url_cidade=url_cidade.replace('/trans/','')
     urls_cidades_fixed.append(url_cidade)
 
 with open('lista_cidades.txt','w') as writer:
